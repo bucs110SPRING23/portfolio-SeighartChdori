9a6763d8a151c2b3c51204c2e8775cce36bb3332
Henghui <WuizWude112@gmail.com>
2023-01-30 17:21:54 -0500

    committed

--- ch01/exercises/data.py
+++ ch01/exercises/data.py
@@ -7,4 +7,4 @@ print (15%10)
 print (10%15)
 print (10%10)
 print (0%10)
-print (10/15) #The printed value is a repeating decimal.
+print (10/15) #The printed value should be either 2/3 or .6 repeating but the printe value is cut off
